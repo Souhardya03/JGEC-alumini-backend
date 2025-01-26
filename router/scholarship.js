b2fda845e4f4fc67077cb19366dd038454afd94e
Swadeshit27 <swadeshpal2002@gmail.com>
2025-01-26 22:15:39 +0530

    init: backend setup completed and register, scholarship done

--- router/scholarship.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require("express");
-const { ScholarshipCreate, getScholarships } = require("../controllers/scholarship");
-const router = Router();
-router.post("/create-scholarship", ScholarshipCreate);
-router.get("/get-scholarship",getScholarships)
-module.exports = router;
